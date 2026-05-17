774206063d56fef97a43c61efc5ff22b524c4591
tsigereda_mitiku <tsigeredamitiku8@gmail.com>
2026-05-17 22:48:33 +0300

    Add files via upload
    Left block  Contribution

---
+++ member1_left_block.cpp
@@ -0,0 +1,13 @@
+void drawLeftBlock() {
+
+    glColor3f(0.0f, 0.0f, 0.0f);
+
+    glBegin(GL_QUADS);
+
+        glVertex2f(-0.7f, -0.6f);
+        glVertex2f(-0.33f, -0.6f);
+        glVertex2f(-0.55f, -0.15f);
+        glVertex2f(-0.75f, -0.45f);
+
+    glEnd();
+}
