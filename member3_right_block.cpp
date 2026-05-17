bebb2bb333e11b71d0ce08c08ced9f866ae48a27
akliluhite <akliluhite@gmail.com>
2026-05-17 11:16:23 -0700

    Add files via upload

---
+++ member3_right_block.cpp
@@ -0,0 +1,13 @@
+void drawRightBlock() {
+
+    glColor3f(0.0f, 0.0f, 0.0f);
+
+    glBegin(GL_QUADS);
+
+        glVertex2f(0.35f, -0.6f);
+        glVertex2f(0.8f, -0.6f);
+        glVertex2f(0.1f, 0.9f);
+        glVertex2f(-0.2f, 0.6f);
+
+    glEnd();
+}
