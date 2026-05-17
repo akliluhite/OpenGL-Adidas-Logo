fdf95d85ff1f73004bc585087ec6ff5cb2a1729e
tsigereda_mitiku <tsigeredamitiku8@gmail.com>
2026-05-18 02:45:45 +0300

    Refactor to include main and display functions

--- member1_left_block.cpp
+++ member1_left_block.cpp
@@ -1,13 +1,41 @@
-void drawLeftBlock() {
+#include <GL/glut.h>
+
+void display() {
+
+    glClear(GL_COLOR_BUFFER_BIT);
 
     glColor3f(0.0f, 0.0f, 0.0f);
 
     glBegin(GL_QUADS);
-
         glVertex2f(-0.7f, -0.6f);
         glVertex2f(-0.33f, -0.6f);
         glVertex2f(-0.55f, -0.15f);
         glVertex2f(-0.75f, -0.45f);
-
     glEnd();
+
+    glFlush();
+}
+
+void init() {
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    gluOrtho2D(-1, 1, -1, 1);
+}
+
+int main(int argc, char** argv) {
+
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+    glutInitWindowSize(700, 500);
+
+    glutCreateWindow("Member 1 - Left Stripe");
+
+    init();
+
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+
+    return 0;
 }
